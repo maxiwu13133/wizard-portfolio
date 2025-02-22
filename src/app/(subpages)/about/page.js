6baae151a40453b9657806b6ba7d42efeb33a224
max <maximilianwu0828@gmail.com>
2025-02-22 15:33:36 -0800

    About page skeleton, Hat model

---
+++ src/app/(subpages)/about/page.js
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import RenderModel from "@/components/RenderModel";
+import bg from "../../../../public/background/about-background.png";
+import Hat from "@/components/models/Hat";
+import AboutDetails from "@/components/about";
+
+export default function Home() {
+  return (
+    <>
+      <Image
+        src={bg}
+        alt="background-image"
+        className="fixed w-full h-full top-0 left-0 object-cover object-center opacity-50 -z-50"
+      />
+
+      <div className="w-full h-screen absolute top-1/2 -translate-y-1/2 left-0">
+        <RenderModel>
+          <Hat />
+        </RenderModel>
+      </div>
+
+      <div className="relative w-full h-screen flex flex-col items-center justify-center">
+        <div className="absolute flex flex-col items-center text-center top-[60%] left-1/2 -translate-y-1/2 -translate-x-1/2">
+          <h2 className="font-bold text-9xl text-accent">Maximilian Wu</h2>
+          <p className="font-light text-foreground text-lg">Meet the wizard behind this portfolio</p>
+        </div>
+      </div>
+
+      <AboutDetails />
+    </>
+  );
+}
---
+++ src/components/about/index.jsx
@@ -0,0 +1,55 @@
+import clsx from "clsx";
+
+const ItemLayout = ({ children, className }) => {
+  return (
+    <div
+      className={clsx(
+        "custom-bg p-8 rounded-xl flex items-center justify-center gap-y-8",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+const AboutDetails = () => {
+  return (
+    <section className="py-20 w-full">
+      <div className="grid grid-cols-12 gap-8 w-full">
+        <ItemLayout className={"col-span-8 row-span-2 flex-col items-start"}>
+          <h2 className="text-2xl text-left w-full capitalize">
+            Architect of Enchantment
+          </h2>
+          <p className="font-light">
+            Joining the dynamic team at Rogers Arena as a Quick Service
+            Supervisor, my journey through the fast-paced world of hospitality
+            management has been nothing short of exhilarating. With a solid
+            educational foundation in Computer Technology from the British
+            Columbia Institute of Technology, I bring a unique blend of
+            technical savvy and customer service excellence to the table. My
+            communication, problem-solving, and time management have been honed
+            over six years in the industry, ensuring seamless guest experience.
+            At the heart of my approach is a commitment to leveraging technology
+            to enhance operational efficiency and guest satisfaction in our
+            bustling arena environment.
+          </p>
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-4 text-accent"}>
+          <p className="font-semibold w-full text-left text-5xl">
+            25+ <sub className="font-semibold text-base">clients</sub>
+          </p>
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-4 text-accent"}>
+          <p className="font-semibold w-full text-left text-5xl">
+            4+ <sub className="font-semibold text-base">years of experience</sub>
+          </p>
+        </ItemLayout>
+      </div>
+    </section>
+  );
+};
+
+export default AboutDetails;
